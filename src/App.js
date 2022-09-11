8c5dfa483f74cd9a903d521d3db70b7d8cb217bd
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2022-09-11 13:28:36 +0545

    added header component and NavMobile component for responsive navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // import components
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Hero from "./components/Hero";
+import Header from "./components/Header";
+import NavMobile from "./components/NavMobile";
 
 // import aos
 import Aos from "aos";
 import "aos/dist/aos.css";
 
 const App = () => {
+  // mobile nav state
+  const [navMobile, setNavMobile] = useState(false);
   // aos init
   useEffect(() => {
     Aos.init({
@@ -15,9 +20,20 @@ const App = () => {
     });
   });
   return (
-    <div>
-      <Hero />
-    </div>
+    <Router>
+      <div className="overflow-hidden">
+        <Header setNavMobile={setNavMobile} />
+        <Hero />
+        {/* mobile nav */}
+        <div
+          className={`${
+            navMobile ? "right-0" : "-right-full"
+          } fixed z-10 top-0 h-full transition-all duration-200`}
+        >
+          <NavMobile setNavMobile={setNavMobile} />
+        </div>
+      </div>
+    </Router>
   );
 };
 
