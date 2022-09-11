7955e0278fd489c63ba92beee3d5bd82376f4f2e
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2022-09-11 14:06:46 +0545

    added Nav and AccountBtns component for Navigation

--- src/components/AccountBtns.js
+++ src/components/AccountBtns.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AccountBtns = () => {
-  return <div>AccountBtns</div>;
+  return (
+    <div className="flex items-center font-medium">
+      {/* link btn */}
+      <Link className="hover:text-blue transition" to="#">
+        Login
+      </Link>
+      {/* separator */}
+      <span className="mx-6 text-white/20 font-thin">|</span>
+      {/* register btn */}
+      <button className="btn h-[52px] text-base px-8">Register</button>
+    </div>
+  );
 };
 
 export default AccountBtns;
